a98aa2fb9581350b3828578856a7e5aa6a7fb9a3
ZacharyGillmore <88333868+ZacharyGillmore@users.noreply.github.com>
2022-01-21 10:14:20 -0800

    put power rule in its own function

--- main.lua
+++ main.lua
@@ -1,9 +1,8 @@
-eq = "11x^3 + 88x^3 + 11y^2 - 66y^1"
+equation = "11x^3 + 88x^3 + 11y^2 - 66y^1"
 
 --TODO list
 	--Format input string to remove fractions and divisions by variable
 	--Return error or reject input if problem cannot be solved with power rule
-	--Put power rule calculater in its own function
 	--Add GUI
 
 function is_term_sep(c)
@@ -16,45 +15,48 @@ function is_term_sep(c)
 	end
 	return false
 end
-eq = string.gsub(eq, " ", "")
+equation = string.gsub(equation, " ", "")
 
-terms = {}
-ops = {}
-term_si = 1
-for i = 1,string.len(eq),1
-do
-	if(is_term_sep(string.sub(eq,i,i))) then
-		table.insert(terms, string.sub(eq,term_si,i-1))
-		table.insert(ops, string.sub(eq,i,i))
-		term_si = i + 1 
-	end
-end
-
-table.insert(terms, string.sub(eq,term_si,string.len(eq)))
-for k,v in pairs(terms)
-do
-	for i = 1,string.len(v)
+function power_rule(eq)
+	local terms = {}
+	local ops = {}
+	local term_si = 1
+	for i = 1,string.len(eq),1
 	do
-		if string.sub(v,i,i) == "^" then
-			num = tonumber(string.sub(v,i+1,-1))*tonumber(string.sub(v,1,i-2))
-			if tonumber(string.sub(v,i+1,-1))-1 == 0 then
-				v = tostring(num)
-			elseif tonumber(string.sub(v,i+1,-1))-1 == 1 then
-				v = tostring(num) .. string.sub(v,i-1,i-1)
-			else
-				v = tostring(num) .. string.sub(v,i-1,i-1) .. "^" ..tostring(tonumber(string.sub(v,i+1,-1))-1)
-			end
-			terms[k] = v
-			break
+		if(is_term_sep(string.sub(eq,i,i))) then
+			table.insert(terms, string.sub(eq,term_si,i-1))
+			table.insert(ops, string.sub(eq,i,i))
+			term_si = i + 1 
 		end
 	end
+
+	table.insert(terms, string.sub(eq,term_si,string.len(eq)))
+	for k,v in pairs(terms)
+	do
+		for i = 1,string.len(v)
+		do
+			if string.sub(v,i,i) == "^" then
+				local num = tonumber(string.sub(v,i+1,-1))*tonumber(string.sub(v,1,i-2))
+				if tonumber(string.sub(v,i+1,-1))-1 == 0 then
+					v = tostring(num)
+				elseif tonumber(string.sub(v,i+1,-1))-1 == 1 then
+					v = tostring(num) .. string.sub(v,i-1,i-1)
+				else
+					v = tostring(num) .. string.sub(v,i-1,i-1) .. "^" ..tostring(tonumber(string.sub(v,i+1,-1))-1)
+				end
+				terms[k] = v
+				break
+			end
+		end
+	end
+	local out = ""
+	local counter = 1
+	for k,v in pairs(ops)
+	do
+		out = out .. terms[k] .. " " .. ops[k] .. " "
+		counter = counter + 1
+	end
+	out = out .. terms[counter]
+	return out
 end
-out = ""
-counter = 1
-for k,v in pairs(ops)
-do
-	out = out .. terms[k] .. " " .. ops[k] .. " "
-	counter = counter + 1
-end
-out = out .. terms[counter]
-print(out)
+print(power_rule(equation))
